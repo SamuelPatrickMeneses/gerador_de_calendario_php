3ccfe4ccf716e71ac96f0a4291ec9ae717a1f54d
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-05-01 13:10:10 -0300

    type: clean file: ./src/app/calendar.php

--- src/app/calendar.php
+++ src/app/calendar.php
@@ -1,56 +1,49 @@
 <?php
     function printRow($start,$wieckStart = 0, $wieckEnd = 7){
-        $wieck = array();
+        echo '<tr>';
 
-        for($i = 0;$i < 7;$i++){
-            $wieck[$i] = ($i < $wieckStart) || ($i >  $wieckEnd) ? '': $start++;
-        }
+        for($i = 0;$i < 7;$i++):
+            $day = ($i < $wieckStart) || ($i >  $wieckEnd) ? '': $start++;
+            echo '<td class="transition-colors hover:bg-orange-600 duration-600">' . $day . '</td>';
+        endfor;
+        echo '</tr>';
+        return $wieckEnd - $wieckStart;
+    }
 
-?>
-        <tr>
-            <td><?php echo $wieck[0];?></td>
-            <td><?php echo $wieck[1];?></td>
-            <td><?php echo $wieck[2];?></td>
-            <td><?php echo $wieck[3];?></td>
-            <td><?php echo $wieck[4];?></td>
-            <td><?php echo $wieck[5];?></td>
-            <td><?php echo $wieck[6];?></td>
-        </tr>
-<?php return $wieckEnd - $wieckStart;}?>
 
-<?php function printMes($start,$end){ ?>
-
-<table>
-    <thead>
-        <tr>
-            <td>Dom</td>
-            <td>Seg</td>
-            <td>Ter</td>
-            <td>Qua</td>
-            <td>Qui</td>
-            <td>Sex</td>
-            <td>Sáb</td>    
-        </tr>
-    </thead>
-    <tbody>
-        <?php   
-            $day = 1;
-            $firstWickSize = printRow($day,$start);
-            $inSet = $end - $firstWickSize; 
-            $modInset = $inSet % 7;
-            $start = 0;
-            $completeWicks = ($inSet  - $modInset) / 7;
-            $day = $firstWickSize +1;
-            for($i = 0;$i < $completeWicks; $i++)
-                $day += printRow($day);
-            $lestWick = ($inSet % 7);
-            if($lestWick)
-                $lestWick = printRow($day,0,--$lestWick);
-           
-        ?>
-    </tbody>
-</table>
-
-<?php 
-return ($lestWick + 1) % 7;
-};?>
+function printMes($start,$end,$name){ 
+    echo '<div class="text-center bg-orange-200 rounded-md">
+            <h4 >'.$name.'</h4>
+            <div class=" bg-orange-400 rounded-md pt-6 h-full">
+                <table class="w-full text-lg">
+                    <thead class="font-bold">
+                        <tr>
+                            <td>Dom</td>
+                            <td>Seg</td>
+                            <td>Ter</td>
+                            <td>Qua</td>
+                            <td>Qui</td>
+                            <td>Sex</td>
+                            <td>Sáb</td>    
+                        </tr>
+                    </thead>
+                    <tbody>';
+                   
+    $day = 1;
+    $firstWickSize = printRow($day,$start);
+    $inSet = $end - $firstWickSize; 
+    $modInset = $inSet % 7;
+    $start = 0;
+    $completeWicks = ($inSet  - $modInset) / 7;
+    $day = $firstWickSize +1;
+    for($i = 0;$i < $completeWicks; $i++)
+        $day += printRow($day);
+    $lestWick = ($inSet % 7);
+    if($lestWick)
+    $lestWick = printRow($day,0,--$lestWick);    
+    echo        '</tbody>
+            </table>
+        </div>
+    </div>';
+    return ($lestWick + 1) % 7;
+};
